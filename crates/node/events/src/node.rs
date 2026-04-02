8c80ff799f41b9d09389589402b383293c7a61cb
Ino Murko <ino.murko.github@protonmail.com>
2026-04-02 23:08:31 +0200

    block log

--- crates/node/events/src/node.rs
+++ crates/node/events/src/node.rs
@@ -262,7 +262,7 @@ impl NodeState {
                 info!(number=block.number(), hash=?block.hash(), ?elapsed, "Block added to fork chain");
             }
             ConsensusEngineEvent::InvalidBlock(block) => {
-                warn!(number=block.number(), hash=?block.hash(), "Encountered invalid block");
+                warn!(number=block.number(), hash=?block.hash(), "DEBUG: Encountered invalid block");
             }
             ConsensusEngineEvent::BlockReceived(num_hash) => {
                 info!(number=num_hash.number, hash=?num_hash.hash, "Received new payload from consensus engine");
